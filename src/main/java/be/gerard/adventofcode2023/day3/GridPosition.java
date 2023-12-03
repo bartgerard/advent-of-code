79fa5b9e99b1aedda3c9099ac0c106bd9fa166d3
Bart Gerard <bart.gerard@acerta.be>
2023-12-03 09:07:38 +0100

    :sparkles: Add Day3 Solution

---
+++ src/main/java/be/gerard/adventofcode2023/day3/GridPosition.java
@@ -0,0 +1,7 @@
+package be.gerard.adventofcode2023.day3;
+
+record GridPosition(
+        int row,
+        int column
+) {
+}
---
+++ src/main/java/be/gerard/adventofcode2023/day3/PartNumber.java
@@ -0,0 +1,13 @@
+package be.gerard.adventofcode2023.day3;
+
+record PartNumber(
+        int row,
+        Range columnRange,
+        int number
+) {
+    boolean adjacentTo(final GridPosition position) {
+        return row - 1 <= position.row()
+                && position.row() <= row + 1
+                && columnRange().adjacentTo(position.column());
+    }
+}
