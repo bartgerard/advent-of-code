1ff15cdf5c3f00235d125b501a12818f6963a789
bartgerard <bart.gerard@gmail.com>
2023-12-06 11:20:07 +0100

    :memo: Add Day6 Documentation

--- aoc-2023/src/main/java/be/gerard/aoc2023/day6/Lap.java
+++ aoc-2023/src/main/java/be/gerard/aoc2023/day6/Lap.java
@@ -1,5 +1,7 @@
 package be.gerard.aoc2023.day6;
 
+import be.gerard.aoc.util.LongRange;
+
 import java.util.stream.LongStream;
 
 record Lap(
@@ -10,18 +12,33 @@ record Lap(
         final long halfTime = duration / 2;
         final boolean isEven = duration % 2 == 0;
 
-        final long recordBreakingStartSpeed = LongStream.rangeClosed(currentRecordDistance / duration + 1, halfTime)
+        final long recordBreakingStartSpeed = LongStream.rangeClosed(minimalRequiredSpeedToBreakRecord(), halfTime)
                 .filter(this::isNewRecord)
                 .findFirst()
                 .orElse(-1);
 
-        if (recordBreakingStartSpeed < 0 && !isEven) {
-            return isNewRecord(duration - halfTime) ? 1 : 0;
+        if (recordBreakingStartSpeed < 0) {
+            return 0;
         }
 
+        //    [1, 2, 3, 4, 5] with >= 2 m/s
+        // -> [   *  *      ]
+
+        //    [1, 2, 3, 4, 5, 6] with >= 2 m/s
+        // -> [   *  *  *      ]
+        // (6 - 3) should not be counted twice
         final long modifier = isEven ? 1 : 0;
 
-        return (halfTime - recordBreakingStartSpeed + 1) * 2 - modifier;
+        return LongRange.of(recordBreakingStartSpeed, halfTime).length() * 2 - modifier;
+    }
+
+    private long currentMaxRecordSpeed() {
+        // 10m in 5s => v_current_max = 2 m/s
+        return currentRecordDistance / duration;
+    }
+
+    private long minimalRequiredSpeedToBreakRecord() {
+        return currentMaxRecordSpeed() + 1;
     }
 
     private boolean isNewRecord(final long startSpeed) {
